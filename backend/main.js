ca4877cff9c777430b541ebdd3b2c1db00fb4cb9
Nic Raboy <nraboy@gmail.com>
2020-10-01 17:52:51 -0700

    push the user profile store backend code from stream 2 and 3

---
+++ backend/main.js
@@ -0,0 +1,69 @@
+const { MongoClient } = require("mongodb");
+const Express = require("express");
+const BodyParser = require('body-parser');
+
+const server = Express();
+const client = new MongoClient("mongodb+srv://<username>:<password>@plummeting-us-east-1.hrrxc.mongodb.net/<dbname>");
+
+server.use(BodyParser.json());
+server.use(BodyParser.urlencoded({ extended: true }));
+
+var collection;
+
+server.post("/plummies", async (request, response, next) => {
+    try {
+        let result = await collection.insertOne(request.body);
+        response.send(result);
+    } catch (e) {
+        response.status(500).send({ message: e.message });
+    }
+});
+
+server.get("/plummies", async (request, response, next) => {
+    try {
+      let result = await collection.find({}).toArray();
+      response.send(result);
+    } catch (e) {
+        response.status(500).send({ message: e.message });
+    }
+});
+
+server.get("/plummies/:plummie_tag", async (request, response, next) => {
+    try {
+        let result = await collection.findOne({ "plummie_tag": request.params.plummie_tag });
+        response.send(result);
+    } catch (e) {
+        response.status(500).send({ message: e.message });
+    }
+});
+
+server.put("/plummies/:plummie_tag", async (request, response, next) => {
+    try {
+        let result = await collection.updateOne(
+            { "plummie_tag": request.params.plummie_tag },
+            { "$set": request.body }
+        );
+        response.send(result);
+    } catch (e) {
+        response.status(500).send({ message: e.message });
+    }
+});
+
+server.delete("/plummies/:plummie_tag", async (request, response, next) => {
+    try {
+        let result = await collection.deleteOne({ "plummie_tag": request.params.plummie_tag });
+        response.send(result);
+    } catch (e) {
+        response.status(500).send({ message: e.message });
+    }
+});
+
+server.listen("3000", async () => {
+    try {
+        await client.connect();
+        collection = client.db("plummeting-people").collection("plummies");
+        console.log("Listening at :3000...");
+    } catch (e) {
+        console.error(e);
+    }
+});
